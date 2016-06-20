499873978cf29d61c4d849015ff0268b7b4d3f33
Max's Ubuntu <maximino@ubuntu.(none)>
2016-06-20 13:49:09 -0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+from Algorithm import *
+
+if __name__ == '__main__':
+    app.run(debug=True)
